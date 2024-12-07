8e6b1699902eeced29b3ba7a29214b25fd9b53e0
Edwin Liang Tang <edwin.liangt@gmail.com>
2024-12-07 17:11:14 -0600

    Solve 2024 Day 06 - Part 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -2,51 +2,68 @@ use std::io;
 use std::error::Error;
 use std::collections::HashSet;
 
+mod walk;
+use walk::*;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin().lock();
     let input: String = io::read_to_string(stdin)?;
     let input: Vec<_> = input.lines().map(str::as_bytes).collect();
 
     println!("Part 1: {}", solve_part_1(&input));
-    //println!("Part 2: {}", solve_part_2(&input));
+    println!("Part 2: {}", solve_part_2(&input));
     Ok(())
 }
 
+
 fn solve_part_1(grid: &[&[u8]]) -> usize {
-    let Some((mut i, mut j)) = find_starting_coords(grid) else {
+    let Some(start) = find_start(grid) else {
         return 0;
     };
 
-    let m = grid.len();
-    let n = grid[0].len();
+    WalkIterator::new(grid, start, Direction::Up)
+        .map(|(i, j, _)| (i, j))
+        .collect::<HashSet<_>>()
+        .len()
+}
 
-    let mut dir = 0;
-    let mut visited = HashSet::new();
-    loop {
-        visited.insert((i, j));
-        let (i2, j2) = match dir {
-            0 => if i != 0  {(i-1, j)} else {break;},
-            1 => if j+1 < n {(i, j+1)} else {break;},
-            2 => if i+1 < m {(i+1, j)} else {break;},
-            3 => if j != 0  {(i, j-1)} else {break;},
-            _ => unreachable!()
-        };
+fn solve_part_2(grid: &[&[u8]]) -> usize {
+    let Some(start) = find_start(grid) else {
+        return 0;
+    };
 
-        if grid[i2][j2] == b'#' {
-            dir = (dir + 1) % 4;
-        } else {
-            (i, j) = (i2, j2)
+    let mut checked_spaces = HashSet::new();
+    let mut new_obstacle_candidates = HashSet::new();
+
+    for (i, j, dir) in WalkIterator::new(grid, start, Direction::Up) {
+        let coords_in_front = dir.offset_coords(i, j);
+        let (i2, j2) = coords_in_front;
+
+        match grid.get(i2).map(|row| row.get(j2)).flatten() {
+            None | Some(&b'#') | Some(&b'^') => continue,
+            _ => {}
+        }
+
+        if checked_spaces.contains(&coords_in_front) {
+            continue;
+        }
+        checked_spaces.insert(coords_in_front);
+
+        if WalkIterator::new(grid, (i, j), dir)
+            .with_added_obstacle(coords_in_front)
+            .is_inifinite_loop()
+        {
+            new_obstacle_candidates.insert(coords_in_front);
         }
     }
 
-    visited.len()
+    new_obstacle_candidates.len()
 }
 
-fn find_starting_coords(grid: &[&[u8]]) -> Option<(usize, usize)> {
+fn find_start(grid: &[&[u8]]) -> Option<(usize, usize)> {
     grid.iter().enumerate()
         .flat_map(|(i, row)| {
-            row.iter().enumerate()
-                .map(move |(j, byte)| (i, j, byte))
+            row.iter().enumerate().map(move |(j, byte)| (i, j, byte))
         })
         .find(|(_, _, &byte)| byte == b'^')
         .map(|(i, j, _)| (i, j))
---
+++ 2024/day06/src/walk.rs
@@ -0,0 +1,93 @@
+use std::collections::HashSet;
+
+pub struct WalkIterator<'a> {
+    grid: &'a [&'a [u8]],
+    i: usize,
+    j: usize,
+    dir: Direction,
+    obstacles: Vec<(usize, usize)>,
+}
+
+impl<'a> Iterator for WalkIterator<'a> {
+    type Item = (usize, usize, Direction);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (i, j, dir) = (self.i, self.j, self.dir);
+        if i >= self.grid.len() || j >= self.grid[i].len() {
+            return None;
+        }
+
+        let (i2, j2) = self.dir.offset_coords(i, j);
+
+        let space_in_front =
+            self.grid.get(i2)
+                .map(|row| row.get(j2))
+                .flatten();
+    
+        if let Some(b'#') = space_in_front {
+            self.dir = dir.rotate();
+        } else if self.obstacles.contains(&(i2, j2)) {
+            self.dir = dir.rotate();
+        } else {
+            self.i = i2;
+            self.j = j2;
+        }
+
+        Some((i, j, dir))
+    }
+}
+
+impl<'a> WalkIterator<'a> {
+    pub fn new(grid: &'a [&'a [u8]], start: (usize, usize), dir: Direction) -> Self {
+        Self{
+            grid,
+            i: start.0,
+            j: start.1,
+            dir,
+            obstacles: vec![]
+        }
+    }
+
+    pub fn with_added_obstacle(mut self, coords: (usize, usize)) -> Self {
+        self.obstacles.push(coords);
+        self
+    }
+
+    pub fn is_inifinite_loop(self) -> bool {
+        let mut seen_states = HashSet::new();
+        for state in self {
+            if seen_states.contains(&state) {
+                return true;
+            }
+            seen_states.insert(state);
+        }
+        false
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {Up, Right, Down, Left}
+impl Direction {
+    pub fn rotate(self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up
+        }
+    }
+
+    pub fn offset_coords(self, i: usize, j: usize) -> (usize, usize) {
+        let i2 = match self {
+            Direction::Up   => i.overflowing_sub(1).0,
+            Direction::Down => i.overflowing_add(1).0,
+            _ => i
+        };
+        let j2 = match self {
+            Direction::Left  => j.overflowing_sub(1).0,
+            Direction::Right => j.overflowing_add(1).0,
+            _ => j
+        };
+        (i2, j2)
+    }
+}
