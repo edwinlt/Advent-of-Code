372372366caaba08e74d0b3c1599b0074f9ebae9
Edwin Liang Tang <edwin.liangt@gmail.com>
2024-12-11 00:14:46 -0600

    Solve 2024 Day 11 - Part 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use std::error::Error;
 use std::iter::successors;
+use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin().lock();
     let input = parse_input(stdin)?;
 
     println!("Part 1: {}", solve_part_1(input.clone()));
-    //println!("Part 2: {}", solve_part_2(input.clone()));
+    println!("Part 2: {}", solve_part_2(input.clone()));
     Ok(())
 }
 
@@ -18,9 +19,8 @@ fn solve_part_1(stones: Vec<usize>) -> usize {
 }
 
 fn solve_part_2(stones: Vec<usize>) -> usize {
-    successors(Some(stones), |stones| Some(apply_rules(stones)))
-        .nth(75).unwrap()
-        .len()
+    let mut memo = HashMap::new();
+    stones.iter().map(|&stone| count_succesors(stone, 75, &mut memo)).sum()
 }
 
 fn apply_rules(stones: &[usize]) -> Vec<usize> {
@@ -42,6 +42,34 @@ fn apply_rules(stones: &[usize]) -> Vec<usize> {
         }
     }
     next
+}
+
+fn count_succesors(stone: usize, depth: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
+    if depth == 0 {
+        return 1;
+    }
+
+    if let Some(val) = memo.get(&(stone, depth)) {
+        return *val;
+    }
+    
+    let num_str = stone.to_string();
+    let digit_count = num_str.len();
+
+    let count = if stone == 0 {
+        count_succesors(1, depth-1, memo)
+    } else if digit_count % 2 == 0 {
+        let (left, right) = num_str.split_at(digit_count/2);
+        let left: usize = left.parse().unwrap();
+        let right: usize = right.parse().unwrap();
+        count_succesors(left, depth-1, memo)
+            + count_succesors(right, depth-1, memo)
+    } else {
+        count_succesors(stone*2024, depth-1, memo)
+    };
+    
+    memo.insert((stone, depth), count);
+    count
 }
 
 fn parse_input(input: impl io::Read) -> Result<Vec<usize>, Box<dyn Error>> {
