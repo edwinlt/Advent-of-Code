d7b76d9a0bc5322dc2f3b1ce6e120dbe43836b6c
Edwin Liang Tang <edwin.liangt@gmail.com>
2024-12-12 22:17:16 -0600

    Refactor 2024/day11

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,74 +1,53 @@
 use std::io;
 use std::error::Error;
-use std::iter::successors;
 use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin().lock();
     let input = parse_input(stdin)?;
 
-    println!("Part 1: {}", solve_part_1(input.clone()));
-    println!("Part 2: {}", solve_part_2(input.clone()));
+    println!("Part 1: {}", solve_part_1(&input));
+    println!("Part 2: {}", solve_part_2(&input));
     Ok(())
 }
 
-fn solve_part_1(stones: Vec<usize>) -> usize {
-    successors(Some(stones), |stones| Some(apply_rules(stones)))
-        .nth(25).unwrap()
-        .len()
-}
-
-fn solve_part_2(stones: Vec<usize>) -> usize {
+fn solve_part_1(stones: &[usize]) -> usize {
     let mut memo = HashMap::new();
-    stones.iter().map(|&stone| count_succesors(stone, 75, &mut memo)).sum()
+    stones.iter().map(|&stone| count_resulting_stones(stone, 25, &mut memo)).sum()
 }
 
-fn apply_rules(stones: &[usize]) -> Vec<usize> {
-    let mut next = vec![];
-    for stone in stones {
-        if *stone == 0 {
-            next.push(1);
-            continue;
-        }
-
-        let num_str = stone.to_string();
-        let digit_count = num_str.len();
-        if digit_count % 2 == 0 {
-            let (left, right) = num_str.split_at(digit_count/2);
-            next.push(left.parse().unwrap());
-            next.push(right.parse().unwrap());
-        } else {
-            next.push(stone * 2024);
-        }
-    }
-    next
+fn solve_part_2(stones: &[usize]) -> usize {
+    let mut memo = HashMap::new();
+    stones.iter().map(|&stone| count_resulting_stones(stone, 75, &mut memo)).sum()
 }
 
-fn count_succesors(stone: usize, depth: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
-    if depth == 0 {
+fn count_resulting_stones(stone: usize, blinks: usize, memo: &mut HashMap<(usize, usize), usize>) -> usize {
+    if blinks == 0 {
         return 1;
     }
 
-    if let Some(val) = memo.get(&(stone, depth)) {
-        return *val;
+    if let Some(&count) = memo.get(&(stone, blinks)) {
+        return count;
     }
-    
-    let num_str = stone.to_string();
-    let digit_count = num_str.len();
 
     let count = if stone == 0 {
-        count_succesors(1, depth-1, memo)
-    } else if digit_count % 2 == 0 {
-        let (left, right) = num_str.split_at(digit_count/2);
-        let left: usize = left.parse().unwrap();
-        let right: usize = right.parse().unwrap();
-        count_succesors(left, depth-1, memo)
-            + count_succesors(right, depth-1, memo)
+        count_resulting_stones(1, blinks-1, memo)
     } else {
-        count_succesors(stone*2024, depth-1, memo)
+        let num_str = stone.to_string();
+        let digit_count = num_str.len();
+
+        if digit_count % 2 == 0 {
+            let (left, right) = num_str.split_at(digit_count/2);
+            let left: usize = left.parse().unwrap();
+            let right: usize = right.parse().unwrap();
+            count_resulting_stones(left, blinks-1, memo)
+                + count_resulting_stones(right, blinks-1, memo)
+        } else {
+            count_resulting_stones(stone*2024, blinks-1, memo)
+        }
     };
-    
-    memo.insert((stone, depth), count);
+
+    memo.insert((stone, blinks), count);
     count
 }
 
